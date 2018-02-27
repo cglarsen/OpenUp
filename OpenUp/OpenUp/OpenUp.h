12c2e06a4baa97e88323835d1d18172d4408b2fb
Christian Graver <christiangraver@gmail.com>
2018-02-27 14:41:42 +0100

    Initial commit

---
+++ OpenUp/OpenUp/OpenUp.h
@@ -0,0 +1,19 @@
+//
+//  OpenUp.h
+//  OpenUp
+//
+//  Created by Christian Graver on 27/02/2018.
+//  Copyright © 2018 Daman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OpenUp.
+FOUNDATION_EXPORT double OpenUpVersionNumber;
+
+//! Project version string for OpenUp.
+FOUNDATION_EXPORT const unsigned char OpenUpVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OpenUp/PublicHeader.h>
+
+
